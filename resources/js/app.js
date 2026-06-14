b9e5ce991dc61ce4cc4262ba02053ae57d1d15e4
Paulette Mbaika <paulette.mbaika@strathmore.edu>
2026-06-14 21:41:54 +0300

    small show

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,11 @@
 // resources/js/app.js
-console.log('Vite: app.js loaded');
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+import axios from 'axios';
+
+window.Alpine = Alpine;
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+Alpine.start();
